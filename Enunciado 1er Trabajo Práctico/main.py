08b298a3c440e6195a02bb52ae48eace0bd08176
Nicolás Gabriel Gallegos <ngallegos@frro.utn.edu.ar>
2024-04-22 14:04:14 -0300

    Actualización algoritmo genético
    #General
    - Se reubicaron los archivos generales para algoritmos genéticos
    #1er Trabajo Práctico:
     - Inciso A terminado

--- Enunciado 1er Trabajo Práctico/main.py
+++ Enunciado 1er Trabajo Práctico/main.py
@@ -1,5 +1,3 @@
-# -*- coding: latin-1 -*-
-#
 # Buscar un máximo de la función:
 # coef = 2^30 - 1
 # f(x) = x/(coef)^2	,	Dom = [0, 2^30 - 1]
@@ -20,145 +18,29 @@
 # - Gráfica de Mín
 # - Gráfica de Promedio
 
-                                # Usado para:
-import os                       # Limpiar la consola
-from random import randint      # Generar numero entero aleatorio
-from random import random       # Generar numero flotante en [0, 1)
-from random import getrandbits  # Generar número entero aleatorio bit a bit
-import numpy as np              # Seleccion de ruleta basada en probabilidades
-from openpyxl import Workbook   # Crear el documento xlsx
-import agxl                     # Modulo propio para manipular rapidamente el documento xlsx
+                                            # Usado para:
+import os                                   # Limpiar la consola
+from openpyxl import Workbook               # Crear el documento xlsx
+import dependencias.agxl as agxl            # Modulo propio para manipular rapidamente el documento xlsx
+import dependencias.pygen as pygen          # Modulo propio para implementar el algoritmo genético
 
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↓↓↓ \ Funciones Debug \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
-def debug_poblacion_inicial(poblacion):
-    for individuo in poblacion:
-        print(str(format(individuo, "010d")) + "\t" + format(individuo, "030b"))
-
-def debug_evaluaciones(evaluaciones):
-    sumatoria = 0
-    for evaluacion in evaluaciones:
-        print(format(evaluacion, ".10f"))
-        sumatoria += evaluacion
-    print("\nSuma total de las evaluaciones: " + str(sumatoria))
-
-def debug_generacion(poblacion, evaluaciones):
-    sumatoria = 0
-    print("Individuo | Cromosoma  | Cromosoma (binario)            | Fitness")
-    for i in range(cantidad_individuos):
-        print("----------+------------+--------------------------------+-------------")
-        print(str(format(i, "9d")) + " | " + str(format(poblacion[i], "010d")) + " | " + format(poblacion[i], "030b") + " | " + format(evaluaciones[i], ".10f"))
-        sumatoria += evaluaciones[i]
-    print("----------+------------+--------------------------------+-------------")
-    print("Total".rjust(9) + " |            |                                | " + format(sumatoria, "012.10f"))
-
-def debug_ciclos(ciclo):
-    porcentaje = int(100 * (ciclo/ciclos))
-    barra = "#" * int(porcentaje/2) + "-" * (50 - int(porcentaje/2))
-    print("Simulacion en curso |" + barra + f"| {ciclo}/{ciclos}", end="\r")
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↑↑↑ \ Funciones Debug \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #    
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↓↓↓ \ Datos iniciales \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
-# Datos de la función
-dominio = [0, pow(2,30)-1]
-coef: float = pow(2, 30) - 1   
-funcion = lambda x : pow(x/coef, 2)
-
 # Datos del algoritmo
+tipo_seleccion: str = "ruleta"
+tipo_crossover: str = "1pto"
+tipo_mutacion: str = "invertida"
 probabilidad_crossover: float = 0.75
 probabilidad_mutacion: float = 0.05
 cantidad_individuos: int = 10
 ciclos: int = 20
 generaciones: int = 20
-tipo_seleccion: str = "ruleta"
-tipo_crossover: str = "1_punto"
-tipo_mutacion: str = "invertida"
+
+# Datos de la función
+dominio: tuple[int, int] = [0, pow(2,30)-1]
+coef: float = pow(2, 30) - 1   
+funcion = lambda x : pow(x/coef, 2)
+
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↑↑↑ \ Datos iniciales \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #   
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↓↓↓ \ Funciones \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
-def generar_poblacion_inicial() -> list[int]:
-    poblacion: list[int] = []
-    for individuo in range(cantidad_individuos):
-        poblacion.append(randint(dominio[0], dominio[1]))                   #| Prueba con randint
-
-        #i: str = ""                                                        #| Prueba con randint bit a bit
-        #for j in range(30):                                                #|
-        #    i += str(randint(0, 1))                                        #|
-        #poblacion.append(int(i, base=2))                                   #|
-        
-        #poblacion.append(getrandbits(30))                                  #| Prueba con getrandbits
-
-        #i: str = ""                                                        #| Prueba con getrandbits bit a bit
-        #for j in range(30):                                                #|
-        #    i += str(getrandbits(1))                                       #|
-        #poblacion.append(int(i, base=2))                                   #|
-
-        #poblacion.append(np.random.randint(dominio[0], dominio[1] + 1))    #| Prueba con numpy randint
-
-        #i: str = ""                                                        #| Prueba con numpy randint bit a bit
-        #for j in range(30):                                                #|
-        #    i += str(np.random.randint(2))                                 #|
-        #poblacion.append(int(i, base=2))                                   #|
-
-    return poblacion
-
-def evaluacion(poblacion: list[int]) -> list[float]:
-    evaluaciones: list[float] = []
-    sumatoria = 0
-    for individuo in poblacion:
-        sumatoria += funcion(individuo)
-    for individuo in poblacion:
-        evaluaciones.append(funcion(individuo)/sumatoria)
-    return evaluaciones
-
-def seleccion(evaluaciones: list[float]) -> list[int]:
-    indices = np.random.choice(a=10, size=2, replace=False, p=evaluaciones)
-    return (indices[0], indices[1])
-
-def recombinacion(padre: int, madre: int) -> list[int]:
-    if random() < probabilidad_crossover:
-        hijo_uno: str = ""
-        hijo_dos: str = ""
-        punto_cruce: int = randint(1, 29)
-        cadena_uno: str = [*str(format(padre, "030b"))]
-        cadena_dos: str = [*str(format(madre, "030b"))]
-        for bit in range(punto_cruce, len(cadena_uno)):
-            auxiliar: str = cadena_uno[bit]
-            cadena_uno[bit] = cadena_dos[bit]
-            cadena_dos[bit] = auxiliar
-        for c in range(len(cadena_uno)):
-            hijo_uno += cadena_uno[c]
-            hijo_dos += cadena_dos[c]
-        return (int(hijo_uno, base=2), int(hijo_dos, base=2))
-    else:
-        return (padre, madre)
-    
-def mutacion(hijos: list[int]) -> list[int]:
-    individuos: list[int] = []
-    for hijo in hijos:
-        if random() < probabilidad_mutacion:
-            #individuo: str = ""
-            #puntos_cruce: int = (randint(1, 29), randint(1, 29))
-            #cadena: str = [*str(format(hijo, "030b"))]
-            #longitud_intercambiar: int = abs(puntos_cruce[0] - puntos_cruce[1])
-            #for desplazamiento in range(int((longitud_intercambiar + 1)/2)):
-            #    auxiliar: str = cadena[min(puntos_cruce) + desplazamiento]
-            #    cadena[min(puntos_cruce) + desplazamiento] = cadena[max(puntos_cruce) - desplazamiento]
-            #    cadena[max(puntos_cruce) - desplazamiento] = auxiliar
-            #for c in range(len(cadena)):
-            #    individuo += cadena[c]
-            #individuos.append(int(individuo, base=2))
-            individuo: str = ""
-            cadena: list[str] = [*str(format(hijo, "030b"))]
-            cromosoma_invertido: int = randint(1, 29)
-            if cadena[cromosoma_invertido] == "0":
-                cadena[cromosoma_invertido] = "1"
-            else:
-                cadena[cromosoma_invertido] = "0"
-            for c in range(len(cadena)):
-                individuo += cadena[c]
-            individuos.append(int(individuo, base=2))
-        else:
-            individuos.append(hijo)
-    return individuos
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↑↑↑ \ Funciones \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #   
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↓↓↓ \ Archivo XLSX \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
 # Definiendo el libro de trabajo (wb: workbook)
 wb = Workbook()
@@ -169,65 +51,34 @@ wb.create_sheet("Ciclos")
 ws_gens = wb["Generaciones"]
 ws_cycles = wb["Ciclos"]
 
-# Formateando la primer fila y la primer columna de las hojas "Generaciones" y "Ciclos"
+# Formateando las hojas "Generaciones" y "Ciclos"
 agxl.generaciones_formateo(ws_gens, ciclos)
 agxl.ciclos_formateo(ws_cycles, generaciones)
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↑↑↑ \ Archivo XLSX \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #   
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↓↓↓ \ Algoritmo Genético \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
-# Programa
 def programa(ciclo: int):
-    finalizar: bool = False
+    ag = pygen.AlgoritmoGenetico(tipo_seleccion, tipo_crossover, tipo_mutacion, probabilidad_crossover, probabilidad_mutacion, cantidad_individuos, ciclos, generaciones, dominio, funcion)
+    ag.generarPoblacionInicial()
+    ag.fitness()
 
-    # Generar población inicial
-    poblacion: list[int] = generar_poblacion_inicial() ### Para debuggear este apartado, utilice 'debug_poblacion_inicial(poblacion)' debajo de esta línea de código
-
-    # Realizar evaluación de cada individuo
-    evaluaciones: list[float] = evaluacion(poblacion) ### Para debuggear este apartado, utilice 'debug_evaluaciones(poblacion)' debajo de esta línea de código
-
-    ### Para visualizar los dos apartados anteriores, utilice 'debug_generacion(poblacion, evaluaciones)' debajo de esta línea de código
-    agxl.generaciones_insertar_tabla(ws_gens, ciclo, 1, poblacion, evaluaciones)
+    agxl.generaciones_insertar_tabla(ws_gens, ciclo, 1, ag.poblacion, ag.list_fitness)
     agxl.generaciones_insertar_grafica(ws_gens, ciclo, 1)
-    
-    # Repetir durante una cierta cantidad de generaciones
     for generacion in range(2, generaciones + 1):
-        nueva_poblacion: list[int] = []
-
-        # Repite por cada pareja de individuos de la población
-        for reproduccion in range(int(cantidad_individuos/2)):
-
-            # Seleccionar un par para intentar realizar el crossover
-            par_seleccionado = seleccion(evaluaciones)
-
-            # Intenta realizar el crossover. Si no tiene éxito, toma a los propios padres como hijos. Luego, agrega a los dos individuos a la lista de la nueva población
-            hijos: list[int] = recombinacion(poblacion[par_seleccionado[0]], poblacion[par_seleccionado[1]])
-
-            # Intenta realizar una mutación sobre los descendientes
-            hijos = mutacion(hijos)
-
-            # Inserta a los nuevos indiviuos a la nueva generación
-            nueva_poblacion.extend(hijos)
-
-        # La nueva generación pasa a ser la población actual
-        poblacion = nueva_poblacion
-
-        # Se realiza la evaluación de la nueva
-        evaluaciones = evaluacion(poblacion)
-
-        agxl.generaciones_insertar_tabla(ws_gens, ciclo, generacion, poblacion, evaluaciones)
+        ag.nuevaGeneracion()
+        ag.fitness()
+        agxl.generaciones_insertar_tabla(ws_gens, ciclo, generacion, ag.poblacion, ag.list_fitness)
         agxl.generaciones_insertar_grafica(ws_gens, ciclo, generacion)
     agxl.ciclos_insertar_tabla(ws_cycles, ciclo, generaciones, cantidad_individuos)
-    agxl.ciclos_insertar_grafica(ws_cycles, ciclo, generaciones)
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↑↑↑ \ Algoritmo Genético \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #  
+    agxl.ciclos_insertar_grafica(ws_cycles, ciclo, generaciones)  
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↑↑↑ \ Algoritmo Genético \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↓↓↓ \ Ejecución \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
-# Iniciar el programa
 os.system("cls")
-print("Iniciando simulacion...") 
-debug_ciclos(0)   
+print("Iniciando simulación...")
 for ciclo in range(ciclos):
+    print(f"Ciclo {ciclo}")
     programa(ciclo)
-    debug_ciclos(ciclo + 1)
-print(f"Simulacion completada. |##################################################| {ciclos}/{ciclos}\nGenerando documento xlsx...")
-wb.save('prueba.xlsx')
-print("Documento creado con exito")
+print("Simulación completada.")
+wb.save("prueba.xlsx")
+print("Documento creado con éxito")
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↑↑↑ \ Ejecución \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #  
---
+++ Modulo General/pygen.py
@@ -0,0 +1,174 @@
+import random   # Librería para generar números aleatorios
+
+class AlgoritmoGenetico:
+    '''Algoritmo Genético'''
+    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↓↓↓ \ Init \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
+
+    def __init__(
+            self,
+            tipo_seleccion: str = 'ruleta',
+            tipo_crossover: str = '1pto',
+            tipo_mutacion: str = 'invertida',
+            prob_crossover: float = 1,
+            prob_mutacion: float = 0,
+            cant_individuos: int = 10,
+            cant_ciclos: int = 20,
+            cant_generaciones: int = 20,
+            dominio: tuple[int, int] = [0, 255],
+            funcion_objetivo = lambda x : x
+            ) -> None:
+        '''
+        Inicializa un Algoritmo Genético con las configuraciones dadas.\n
+        Parámetros por defecto
+        --
+        - Tipo Seleccion: Ruleta
+        - Tipo Crossover: 1 Punto
+        - Tipo Mutacion: Invertida
+        - Prob. Crossover: 100%
+        - Prob. Mutacion: 0%
+        - Cant. Individuos: 20
+        - Cant. Ciclos: 20
+        - Cant. Generaciones: 20
+        - Dominio: [0, 2^8 - 1]
+        - Funcion Objetivo: f(x) = x
+        '''
+        self.tipo_seleccion: str = tipo_seleccion
+        self.tipo_crossover: str = tipo_crossover
+        self.tipo_mutacion: str = tipo_mutacion
+        self.prob_crossover: float = prob_crossover
+        self.prob_mutacion: float = prob_mutacion
+        self.cant_individuos: int = cant_individuos
+        self.cant_ciclos: int = cant_ciclos
+        self.cant_generaciones: int = cant_generaciones
+        self.dominio: tuple[int, int] = dominio
+        self.poblacion: list[int] = []
+        self.poblacion_siguiente: list[int] = []
+        self.list_fitness: list[float] = []
+        self.funcion_objetivo = funcion_objetivo
+      
+    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↑↑↑ \ Init \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #   
+    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↓↓↓ \ Setters y Getters \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
+    def setTipoSeleccion(self, tipo_seleccion: str) -> None: self.tipo_seleccion = tipo_seleccion
+    
+    def setTipoCrossover(self, tipo_crossover: str) -> None: self.tipo_crossover = tipo_crossover
+    
+    def setTipoMutacion(self, tipo_mutacion: str) -> None: self.tipo_mutacion = tipo_mutacion
+    
+    def setProbCrossover(self, prob_crossover: float) -> None: self.prob_crossover = prob_crossover
+    
+    def setProbMutacion(self, prob_mutacion: float) -> None: self.prob_mutacion = prob_mutacion   
+    
+    def setCantIndividuos(self, cant_individuos: int) -> None: self.cant_individuos = cant_individuos       
+    
+    def setCantCiclos(self, cant_ciclos: int) -> None: self.cant_ciclos = cant_ciclos       
+    
+    def setCantGeneraciones(self, cant_generaciones: int) -> None: self.cant_generaciones = cant_generaciones            
+    
+    def setDominio(self, dominio: tuple[int, int]) -> None: self.dominio = dominio
+
+    def setFuncionObjetivo(self, funcion_objetivo) -> None: self.funcion_objetivo = funcion_objetivo
+
+    def getIndividuo(self, indice: int) -> int: return self.poblacion[indice]
+ 
+    def getSumaIndividuos(self) -> int:
+        '''Devuelve la suma de los valores de todos cada individuo de la población'''
+        suma: int = 0
+        for individuo in self.poblacion:
+            suma += individuo
+        return suma
+
+    def getSumaFunciones(self) -> float:
+        '''Devuelve la suma de las funciones objetivos de cada individuo de población'''
+        suma: float = 0
+        for individuo in self.poblacion:
+            suma += self.funcion_objetivo(individuo)
+        return suma
+    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↑↑↑ \ Setters y Getters \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #   
+    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↓↓↓ \ Funciones Principales \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #   
+    def generarPoblacionInicial(self) -> None:
+        '''Genera la población inicial del Algoritmo Genético'''
+        poblacion: list[int] = []
+        for individuo in range(self.cant_individuos):
+            poblacion.append(random.randint(self.dominio[0], self.dominio[1]))
+        self.poblacion: list[int] = poblacion.copy()
+
+    def fitness(self) -> None:
+        '''Calcula el valor de cada individuo dentro de la población'''
+        list_fitness: list[float] = []
+        for individuo in self.poblacion:
+            list_fitness.append(self.funcion_objetivo(individuo)/self.getSumaFunciones())
+        self.list_fitness: list[int] = list_fitness.copy()
+
+    def selection(self) -> int:
+        '''Devuelve un individuo utilizando el método de selección del Algoritmo Genético'''
+        match self.tipo_seleccion:
+            case 'ruleta':
+                pelota: float = random.random()
+                acum: float = 0
+                for i in range(self.cant_individuos):
+                    acum += self.list_fitness[i]
+                    if acum > pelota:
+                        return self.poblacion[i]
+    
+    def crossover(self, padre: int, madre: int) -> tuple[int, int]:
+        '''Cruza (o no) la información genética de dos individuos y devuelve dos nuevos individuos'''
+        if random.random() < self.prob_crossover:
+            hijo_uno: str = ""
+            hijo_dos: str = ""
+            bits_hijo_uno: str = [*format(padre, "030b")]
+            bits_hijo_dos: str = [*format(madre, "030b")]
+            punto_de_cruce: int = random.randint(1, len(bits_hijo_uno) - 1)
+            match self.tipo_crossover:
+                case '1pto':
+                    for bit in range(punto_de_cruce, len(bits_hijo_uno)):
+                        bit_auxiliar: str = bits_hijo_uno[bit]
+                        bits_hijo_uno[bit] = bits_hijo_dos[bit]
+                        bits_hijo_dos[bit] = bit_auxiliar
+                    for bit in range(len(bits_hijo_uno)):
+                        hijo_uno += bits_hijo_uno[bit]
+                        hijo_dos += bits_hijo_dos[bit]
+                    print("Return verdadero")
+                    return(int(hijo_uno, base=2), int(hijo_dos, base=2))
+        else:
+            print("Return falso")
+            return (padre, madre)
+
+    def mutation(self, *individuos: int) -> list[int]:
+        '''Muta (o no) y devuelve a los individuos de la población pasados como parámetros'''
+        individuos_mutados: list[int] = []
+        for individuo in individuos:
+            if random.random() < self.prob_mutacion:
+                match self.tipo_mutacion:
+                    case 'invertida':
+                        individuo_auxiliar: str = ""
+                        bits_individuo: list[str] = [*format(individuo, "030b")]
+                        bit_inveritdo: int = random.randint(1, len(bits_individuo) - 1)
+                        bits_individuo[bit_inveritdo] = "1" if bits_individuo[bit_inveritdo] == "0" else "0"
+                        for bit in range(len(bits_individuo)):
+                            individuo_auxiliar += bits_individuo[bit]
+                        individuos_mutados.append(int(individuo_auxiliar, base=2))
+            else:
+                individuos_mutados.append(individuo)
+        return individuos_mutados
+    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↑↑↑ \ Funciones Principales \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #   
+    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↓↓↓ \ Funciones Adicionales \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #   
+    def nuevaGeneracion(self):
+        for i in range(int(self.cant_individuos/2)):
+            self.cicloReproductivo()
+        for individuo in range(self.cant_individuos):
+            print(f"{individuo}\n{self.poblacion}\n{self.poblacion_siguiente}\n\n")
+            self.poblacion[individuo] = self.poblacion_siguiente[individuo]
+        self.poblacion_siguiente = []
+    
+    def cicloReproductivo(self):
+        padre, madre = self.selection(), self.selection()   # Seleccionar
+        hijos = self.crossover(padre, madre)                # Cruzar
+        hijos = self.mutation(*hijos)                       # Mutar
+        print(f"Hijos: {hijos}\n")
+        self.poblacion_siguiente.extend(hijos)             # Insertar
+    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↑↑↑ \ Funciones Adicionales \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #   
+
+
+    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↓↓↓ \ X \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #   
+    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- ↑↑↑ \ X \ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #  
+
